33c4e6f1684978c6f76c06d20e8687636d80244f
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-24 23:42:55 +0800

    Update ESP32AppSegment.java

--- src/main/java/esp32_loader/flash/ESP32AppSegment.java
+++ src/main/java/esp32_loader/flash/ESP32AppSegment.java
@@ -1,98 +1,166 @@
 package esp32_loader.flash;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ghidra.app.util.bin.BinaryReader;
 
-public class ESP32AppSegment {
+public class UnknownModelException extends Exception { 
+    public UnknownModelException(String errorMessage) {
+        super(errorMessage);
+    }
+}
 
-	public enum SegmentType {
-		DRAM0, DRAM1, IF_TXT, IRAM0, IRAM1, DF_ROA, F_DATA, IRAM, IROM0, DROM0
+public class ESP32AddressRange {
+	public int StartAddress;
+	public int EndAddress;
+	public boolean Writeable;
+	public boolean Executable;
+}
+
+public class ESP32AddressSpace {
+	private List<ESP32AddressRange> ESP32AddressRangeList = new ArrayList<ESP32AddressRange>();
+	private void SetAddressRangePermissions(int StartAddress, int EndAddress, boolean Writeable, boolean Executable) {
+		ESP32AddressRangeList.add(new ESP32AddressRange(StartAddress, EndAddress, Writeable, Executable));
 	}
+	public ESP32AddressSpace(short chipID) throws UnknownModelException {
+		switch(chipID) { // based on the technical reference manuals of the respective chips
+			case 0: // ESP32
+				// internal
+				this.SetAddressRangePermissions(0x3FF9_0000, 0x3FF9_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3FFA_E000, 0x3FFD_FFFF, true, false);
+				this.SetAddressRangePermissions(0x3FFE_0000, 0x3FFF_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4000_0000, 0x4000_7FFF, false, true);
+				this.SetAddressRangePermissions(0x4000_8000, 0x4005_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4007_0000, 0x4007_FFFF, true, true);
+				this.SetAddressRangePermissions(0x4008_0000, 0x4009_FFFF, true, true);
+				this.SetAddressRangePermissions(0x400A_0000, 0x400A_FFFF, true, true);
+				this.SetAddressRangePermissions(0x400B_0000, 0x400B_7FFF  true, true);
+				this.SetAddressRangePermissions(0x400B_8000, 0x400B_FFFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x3F40_0000, 0x3F7F_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3F80_0000, 0x3FBF_FFFF, true, false);
+				this.SetAddressRangePermissions(0x400C_2000, 0x40BF_FFFF, true, true);
+				break
+			case 2: // ESP32-S2
+				// internal
+				this.SetAddressRangePermissions(0x3FFA_0000, 0x3FFA_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3FFB_0000, 0x3FFB_7FFF, true, false);
+				this.SetAddressRangePermissions(0x3FFB_8000, 0x3FFF_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4000_0000, 0x4000_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4001_0000, 0x4001_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4002_0000, 0x4002_7FFF, true, true);
+				this.SetAddressRangePermissions(0x4002_8000, 0x4006_FFFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x3F00_0000, 0x3F3F_FFFF, true, false);
+				this.SetAddressRangePermissions(0x3F50_0000, 0x3FF7_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4008_0000, 0x407F_FFFF, true, true);
+				break
+			case 9: // ESP32-S3
+				// internal
+				this.SetAddressRangePermissions(0x3FF0_0000, 0x3FF1_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3FC8_8000, 0x3FCE_FFFF, true, false);
+				this.SetAddressRangePermissions(0x3FCF_0000, 0x3FCF_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4000_0000, 0x4003_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4004_0000, 0x4005_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4037_0000, 0x4037_7FFF, true, true);
+				this.SetAddressRangePermissions(0x4037_8000, 0x403D_FFFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x3C00_0000, 0x3DFF_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4200_0000, 0x43FF_FFFF, true, true);
+				break
+			case 12: // ESP32-C2
+				// internal
+				this.SetAddressRangePermissions(0x3FF0_0000, 0x3FF4_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3FCA_0000, 0x3FCD_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4000_0000, 0x4003_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4004_0000, 0x4008_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4037_C000, 0x4037_FFFF, true, true);
+				this.SetAddressRangePermissions(0x4038_0000, 0x403B_FFFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x3C00_0000, 0x3C3F_FFFF, false, false);
+				this.SetAddressRangePermissions(0x4200_0000, 0x423F_FFFF, true, true);
+				break
+			case 5: // ESP32-C3
+				// internal
+				this.SetAddressRangePermissions(0x3FF0_0000, 0x3FF1_FFFF, false, false);
+				this.SetAddressRangePermissions(0x3FC8_0000, 0x3FCD_FFFF, true, false);
+				this.SetAddressRangePermissions(0x4000_0000, 0x4003_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4004_0000, 0x4005_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4037_C000, 0x4037_FFFF, true, true);
+				this.SetAddressRangePermissions(0x4038_0000, 0x403D_FFFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x3C00_0000, 0x3C7F_FFFF, false, false);
+				this.SetAddressRangePermissions(0x4200_0000, 0x427F_FFFF, true, true);
+				break
+			case 13: // ESP32-C6
+			case 20:
+				// internal
+				this.SetAddressRangePermissions(0x4000_0000, 0x4004_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4080_0000, 0x4087_FFFF, true, true);
+				this.SetAddressRangePermissions(0x5000_0000, 0x5000_3FFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x4200_0000, 0x42FF_FFFF, true, true);
+				break
+			case 16: // ESP32-H2
+				// internal
+				this.SetAddressRangePermissions(0x4000_0000, 0x4001_FFFF, false, true);
+				this.SetAddressRangePermissions(0x4080_0000, 0x4084_FFFF, true, true);
+				this.SetAddressRangePermissions(0x5000_0000, 0x5000_0FFF, true, true);
+				// external
+				this.SetAddressRangePermissions(0x4200_0000, 0x42FF_FFFF, true, true);
+			default:
+				throw new UnknownModelException("Unknown ESP32 Chip ID : " + chipID );
+		}
+	}
+	private int GetAddressRangePermissions(int address) {
+		private boolean addressRangeFound = false;
+		private boolean addressRangeWriteable = false;
+		private boolean addressRangeExecutable = false;
+		for (ESP32AddressRange addressRange : ESP32AddressRangeList) {
+			if (address >= addressRange.StartAddress && address <= addressRange.EndAddress) {
+				addressRangeWriteable = addressRange.Writeable;
+				addressRangeExecutable = addressRange.Executable;
+				break
+			}
+		}
+		if (!addressRangeFound) {
+			addressRangeWriteable = true;
+			addressRangeExecutable = true;
+		}
+		return (addressRangeWriteable ? 1 : 0) + (addressRangeExecutable ? 2 : 0);
+	}
+}
+
+public class ESP32AppSegment {
 
 	public int PhysicalOffset;
 	public int LoadAddress;
 	public int Length;
 	public byte[] Data;
+	public int Permissions;
 
-	public SegmentType type;
-	public boolean IsEsp32 = false;
-
-	public ESP32AppSegment(ESP32AppImage app, BinaryReader reader, boolean isEsp32S2) throws IOException {
-		// TODO Auto-generated constructor stub
-		IsEsp32 = isEsp32S2;
+	public ESP32AppSegment(ESP32AppImage app, BinaryReader reader, ESP32AddressSpace addressSpace) throws IOException {
 		LoadAddress = reader.readNextInt();
 		Length = reader.readNextInt();
 		Data = reader.readNextByteArray(Length);
-		/* fully consume the segment */
-		if (isEsp32S2) {
-			/* determine access type via memory map */
-			/*
-			 * Loading section .flash.rodata, size 0x576c lma 0x3f000020 DROM0 Loading
-			 * section .dram0.data, size 0x1e74 lma 0x3ffbe150 Loading section
-			 * .iram0.vectors, size 0x403 lma 0x40024000 Loading section .iram0.text, size
-			 * 0x9d40 lma 0x40024404 Loading section .flash.text, size 0x147f7 lma
-			 * 0x40080020
-			 */
-			if (LoadAddress >= 0x3FFB0000 && LoadAddress <= 0x3FFB7FFF) {
-				type = SegmentType.DRAM0;
-			} else if (LoadAddress >= 0x3FFB8000 && LoadAddress <= 0x3FFFFFFF) {
-				type = SegmentType.DRAM1;
-			} else if (LoadAddress >= 0x40080000 && LoadAddress <= 0x40080000 + 4194304) {
-				type = SegmentType.IF_TXT;
-			} else if (LoadAddress >= 0x40020000 && LoadAddress <= 0x40027FFF) {
-				type = SegmentType.IRAM0;
-			} else if (LoadAddress >= 0x40028000 && LoadAddress <= 0x4006FFFF) {
-				type = SegmentType.IRAM1;
-			} else if (LoadAddress >= 0x3F000000 && LoadAddress <= 0x3F3F0000) {
-				type = SegmentType.DF_ROA;
-			} else if (LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
-				type = SegmentType.F_DATA;
-			} else {
-				type = SegmentType.IRAM;
-			}
-
-		} else {
-			/* determine access type via memory map */
-			if (LoadAddress >= 0x40800000 && LoadAddress <= 0x40800000 + 4194304) {
-				type = SegmentType.IROM0;
-			} else if (LoadAddress >= 0x40000000 && LoadAddress <= 0x40000000 + 4194304) {
-				type = SegmentType.IRAM0;
-			} else if (LoadAddress >= 0x40400000 && LoadAddress <= 0x40400000 + 4194304) {
-				type = SegmentType.IRAM1;
-			} else if (LoadAddress >= 0x3F400000 && LoadAddress <= 0x3F400000 + 4194304) {
-				type = SegmentType.DROM0;
-			} else if (LoadAddress >= 0x3FF80000 && LoadAddress <= 0x3FF80000 + 524288) {
-				type = SegmentType.DRAM0;
-			} else if (LoadAddress >= 0x3F800000 && LoadAddress <= 0x3F800000 + 4194304) {
-				type = SegmentType.DRAM1;
-			} else {
-				type = SegmentType.IRAM;
-			}
-		}
-	}
-
-	public boolean isWrite() {
-		// There are definitely code segments that are writable, but it seems like it is
-		// common for the compiler to stick function pointers in the code
-		// segments. This means the decompiler will show the pointer being casted to
-		// `code *` and called, making it more difficult to read. If the
-		// pointers are not writable, then the decompiler will helpfully show a function
-		// pointer call as a normal function call.
-		return this.type != null && this.type != SegmentType.DROM0 && this.type != SegmentType.IROM0
-				&& !this.isCodeSegment();
+		Permissions = addressSpace.GetAddressRangePermissions(LoadAddress)
 	}
 
 	public boolean isRead() {
-		return this.type != null && this.type != SegmentType.F_DATA;
+		return true;
+	}
+
+	public boolean isWrite() {
+		return ((this.Permissions & 1) != 0);
 	}
 
 	public boolean isExecute() {
-		return this.isCodeSegment();
+		return ((this.Permissions & 1) != 0);
 	}
 
 	public boolean isCodeSegment() {
-		return this.type != null && (this.type == SegmentType.IF_TXT || this.type == SegmentType.IRAM
-				|| this.type == SegmentType.IRAM0 || this.type == SegmentType.IRAM1 || this.type == SegmentType.IROM0);
+		return this.isExecute();
 	}
 }
