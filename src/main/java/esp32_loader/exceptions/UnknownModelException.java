3cdbbbe38e22530675951877e83795f55b38a3df
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-25 18:18:08 +0800

    Create UnknownModelException.java

---
+++ src/main/java/esp32_loader/exceptions/UnknownModelException.java
@@ -0,0 +1,7 @@
+package esp32_loader.exceptions;
+
+public class UnknownModelException extends Exception { 
+    public UnknownModelException(String errorMessage) {
+        super(errorMessage);
+    }
+} 
