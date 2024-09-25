16607cd6ab240cba2da243b1b4f208aeb27c64d1
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-25 16:17:18 +0800

    Update esp32_loaderLoader.java

--- src/main/java/esp32_loader/esp32_loaderLoader.java
+++ src/main/java/esp32_loader/esp32_loaderLoader.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.*;
 
 import esp32_loader.flash.ESP32Flash;
 import esp32_loader.flash.ESP32Partition;
