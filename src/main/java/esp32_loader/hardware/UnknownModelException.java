b3fdb0c96244d6711356ab588c7045830c2f6a8c
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-27 13:58:28 +0800

    Delete src/main/java/esp32_loader/hardware/UnknownModelException.java

--- src/main/java/esp32_loader/hardware/UnknownModelException.java
+++
@@ -1,7 +0,0 @@
-package esp32_loader.hardware;
-
-public class UnknownModelException extends Exception { 
-    public UnknownModelException(String errorMessage) {
-        super(errorMessage);
-    }
-} 
