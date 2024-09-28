254bb001dd808abe385b2faa984abd16572c0581
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-28 21:24:29 +0800

    Update ESP32Chip.java

--- src/main/java/esp32_loader/hardware/ESP32Chip.java
+++ src/main/java/esp32_loader/hardware/ESP32Chip.java
@@ -12,19 +12,20 @@ public class ESP32Chip {
 		public String chipModel;
 		public String chipSubmodel;
 		public String chipProcessor;
+		public boolean isApproximation;
 
-		public ChipData(String chipModel, String chipSubmodel, String chipProcessor) {
+		public ChipData(String chipModel, String chipSubmodel, String chipProcessor, boolean isApproximation;) {
 			this.chipModel = chipModel;
 			this.chipSubmodel = chipSubmodel;
 			this.chipProcessor = chipProcessor;
+			this.isApproximation = isApproximation;
 		}
 	}
 	
 	public ChipData chipData;
 	public ESP32ChipMappings chipMappings;
-	public boolean isApproximation;
 
-	public ESP32Chip(short chipID) {
+	static ChipData lookup(short chipID) {
 		try {
 			String chipModel;
 			String chipSubmodel;
@@ -40,30 +41,33 @@ public class ESP32Chip {
 	
 			NodeList chipList = root.getElementsByTagName("chip");
 			Element chipInfo = (Element) chipList.item(0);
-			int diff = chipID;
+			int prevDiff = chipID;
 			for (var x = 0; x < chipList.getLength(); x++) {
 				Node chipNode = chipList.item(i);
 				Element chipElement = (Element) chipNode;
-				int newDiff = Math.abs(short.valueOf(chipElement.getAttribute("id")) - chipID);
-				if (newDiff < diff) {
+				int curDiff = Math.abs(short.valueOf(chipElement.getAttribute("id")) - chipID);
+				if (curDiff < prevDiff) {
 					chipInfo = chipElement;
-					diff = newDiff;
+					prevDiff = curDiff;
 				}
-				if (newDiff == 0) {
+				if (curDiff == 0) {
 					break;
 				}
 			}
 			chipModel = (String) chipInfo.getElementsByTagName("model");
 			chipSubmodel = (String) chipInfo.getElementsByTagName("submodel");
 			chipProcessor = (String) chipInfo.getElementsByTagName("processor");
-			chipData = new ChipData(chipModel, chipSubmodel, chipProcessor);
-			isApproximation = (newDiff == 0);
+			isApproximation = (curDiff == 0);
+			return new ChipData(chipModel, chipSubmodel, chipProcessor, isApproximation);
 		} catch (Exception e) {
 			String exceptionTxt = e.toString();
 			System.out.println(exceptionTxt);
-			chipData = new ChipData("esp32", "esp32", "Xtensa:LE:32:default");
-			isApproximation = true;
+			return new ChipData("esp32", "esp32", "Xtensa:LE:32:default", true);
 		}
+	}
+
+	public ESP32Chip(short chipID) {
+		chipData = this.lookup(chipID);
 		try {
 			chipMappings = new ESP32ChipMappings(chipData);
 		} catch (Exception e) {
