6b4d48074f6ef43767242f6b17d8d00d2b948be8
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-29 20:53:28 +0800

    Update ESP32ChipPeripherals.java

--- src/main/java/esp32_loader/hardware/ESP32ChipPeripherals.java
+++ src/main/java/esp32_loader/hardware/ESP32ChipPeripherals.java
@@ -34,30 +34,32 @@ public class ESP32ChipPeripherals {
 
 		String peripheralName = ((Element) (peripheral.getElementsByTagName("name").item(0))).getTextContent();
 		Element addressBlock = (Element) peripheral.getElementsByTagName("addressBlock").item(0);
-		int size = Integer.decode(addressBlock.getElementsByTagName("size").item(0).getTextContent());
-
-		StructureDataType struct = new StructureDataType(peripheralName, size);
-		StandAloneDataTypeManager tempDTMgr = new StandAloneDataTypeManager(peripheralName);
-
-		NodeList registers = peripheral.getElementsByTagName("register");
-
-		for (var x = 0; x < registers.getLength(); x++) {
-			Element register = (Element) registers.item(x);
-			String registerName = ((Element) (register.getElementsByTagName("name").item(0))).getTextContent();
-			String offsetString = ((Element) (register.getElementsByTagName("addressOffset").item(0))).getTextContent();
-			int offsetValue = Integer.decode(offsetString);
-			String registerSizeString = ((Element) (register.getElementsByTagName("size").item(0))).getTextContent();
-			int registerSizeValue = Integer.decode(registerSizeString) >> 3; // bits to bytes
-			// sometimes the SVD file implies that end of a register (offset + registerSizeValue) is greater than the struct
-			// size (i.e. part of the register field lies outside of the struct)??
-			if (offsetValue + registerSizeValue > size) {
-				struct.growStructure​(offsetValue + registerSizeValue - size);
-				size = offsetValue + registerSizeValue;
+		if (addressBlock != null) {
+			int size = Integer.decode(addressBlock.getElementsByTagName("size").item(0).getTextContent());
+	
+			StructureDataType struct = new StructureDataType(peripheralName, size);
+			StandAloneDataTypeManager tempDTMgr = new StandAloneDataTypeManager(peripheralName);
+	
+			NodeList registers = peripheral.getElementsByTagName("register");
+	
+			for (var x = 0; x < registers.getLength(); x++) {
+				Element register = (Element) registers.item(x);
+				String registerName = ((Element) (register.getElementsByTagName("name").item(0))).getTextContent();
+				String offsetString = ((Element) (register.getElementsByTagName("addressOffset").item(0))).getTextContent();
+				int offsetValue = Integer.decode(offsetString);
+				String registerSizeString = ((Element) (register.getElementsByTagName("size").item(0))).getTextContent();
+				int registerSizeValue = Integer.decode(registerSizeString) >> 3; // bits to bytes
+				// sometimes the SVD file implies that end of a register (offset + registerSizeValue) is greater than the struct
+				// size (i.e. part of the register field lies outside of the struct)??
+				if (offsetValue + registerSizeValue > size) {
+					struct.growStructure​(offsetValue + registerSizeValue - size);
+					size = offsetValue + registerSizeValue;
+				}
+				struct.replaceAtOffset(offsetValue, AbstractIntegerDataType.getUnsignedDataType(registerSizeValue, tempDTMgr),
+						       registerSizeValue, registerName, "");
 			}
-			struct.replaceAtOffset(offsetValue, AbstractIntegerDataType.getUnsignedDataType(registerSizeValue, tempDTMgr),
-					       registerSizeValue, registerName, "");
+			chipPeripheralsList.add(new ESP32ChipPeripheral(baseAddr, peripheralName, struct, size));
 		}
-		chipPeripheralsList.add(new ESP32ChipPeripheral(baseAddr, peripheralName, struct, size));
 	}
 
 	public ESP32ChipPeripherals(ESP32Chip.ChipData chipData) throws Exception {
