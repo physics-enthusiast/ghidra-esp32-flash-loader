82560864863f71410445dcc09dd1bc4b18617a5e
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-09-29 20:07:20 +0800

    Update ESP32ChipPeripherals.java

--- src/main/java/esp32_loader/hardware/ESP32ChipPeripherals.java
+++ src/main/java/esp32_loader/hardware/ESP32ChipPeripherals.java
@@ -5,7 +5,8 @@ import java.util.*;
 import generic.jar.ResourceFile;
 import ghidra.framework.Application;
 import ghidra.program.model.data.StructureDataType;
-import ghidra.program.model.data.UnsignedLongDataType;
+import ghidra.program.model.data.AbstractIntegerDataType;
+import ghidra.program.model.data.StandAloneDataTypeManager;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 
@@ -36,6 +37,7 @@ public class ESP32ChipPeripherals {
 		int size = Integer.decode(addressBlock.getElementsByTagName("size").item(0).getTextContent());
 
 		StructureDataType struct = new StructureDataType(peripheralName, size);
+		StandAloneDataTypeManager tempDTMgr = new StandAloneDataTypeManager(peripheralName);
 
 		NodeList registers = peripheral.getElementsByTagName("register");
 
@@ -44,7 +46,16 @@ public class ESP32ChipPeripherals {
 			String registerName = ((Element) (register.getElementsByTagName("name").item(0))).getTextContent();
 			String offsetString = ((Element) (register.getElementsByTagName("addressOffset").item(0))).getTextContent();
 			int offsetValue = Integer.decode(offsetString);
-			struct.replaceAtOffset(offsetValue, new UnsignedLongDataType(), 4, registerName, "");
+			String registerSizeString = ((Element) (register.getElementsByTagName("size").item(0))).getTextContent();
+			int registerSizeValue = Integer.decode(registerSizeString) >> 3; // bits to bytes
+			// sometimes the SVD file implies that end of a register (offset + registerSizeValue) is greater than the struct
+			// size (i.e. part of the register field lies outside of the struct)??
+			if (offsetValue + registerSizeValue > size) {
+				struct.growStructure​(offsetValue + registerSizeValue - size);
+				size = offsetValue + registerSizeValue;
+			}
+			struct.replaceAtOffset(offsetValue, AbstractIntegerDataType.getUnsignedDataType(registerSizeValue, tempDTMgr),
+					       registerSizeValue, registerName, "");
 		}
 		chipPeripheralsList.add(new ESP32ChipPeripheral(baseAddr, peripheralName, struct, size));
 	}
